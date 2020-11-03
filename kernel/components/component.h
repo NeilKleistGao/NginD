4fc722414c8bd4b8360a1d06156e2df6e18c630c
NeilKleistGao <neilkleistgao@163.com>
2020-11-03 09:50:27 +0800

    add comment of component

--- kernel/components/component.h
+++ kernel/components/component.h
@@ -20,7 +20,12 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // FILENAME: component.h
-// LAST MODIFY: 2020/10/31
+// LAST MODIFY: 2020/11/3
+
+// This file includes the basic component class. Include this file if you need to
+// create your own component classes.
+// This file also includes a forward declaration. If you modified the object class,
+// you may need to clean and recompile this part.
 
 #ifndef NGIND_COMPONENT_H
 #define NGIND_COMPONENT_H
@@ -32,27 +37,52 @@
 
 namespace ngind {
 
+// A forward declaration to prevent from circle including.
 class Object;
 
+// Basic class of all components. You can create an instance either by newing or
+// by create/init functions. We do not allow user to create a basic component in
+// configuration files so create function is not declared.
 class Component : public UpdatableObject {
 public:
+    // Default constructor function
     Component() = default;
+
+    // Default destructor function
     virtual ~Component() = default;
+
+    // Copy behavior is not allowed. You can only maintain a pointer of a component
     Component(const Component&) = delete;
     Component& operator= (const Component&) = delete;
 
+    // Update the state of this component. This function is inherited from UpdatableObject
+    // @see objects/updatable_object.h
     virtual void update(const float&) {}
-    virtual void init(const typename ConfigResource::JsonObject&) {};
 
+    // Initialization function of this class used by configuration creating method.
+    // params:
+    //      const typename ConfigResource::JsonObject& : the configuration data
+    //      this component initialization process requires.
+    // return: void
+    virtual void init(const typename ConfigResource::JsonObject&) {}
+
+    // Get the parent object of this component.
+    // params: void
+    // return : Object*, the parent object's pointer
     inline Object* getParent() const {
         return _parent;
     }
 
+    // Set the parent object of this component.
+    // params:
+    //      Object* parent : the parent object's pointer
+    // return: void
     inline void setParent(Object* parent) {
         _parent = parent;
     }
 
 protected:
+    // Parent object of this component
     Object* _parent;
 };
 
