9f25dbd5abea921dc7e0e2bf94f346b8fbbeedbb
NeilKleistGao <neilkleistgao@163.com>
2021-01-24 13:15:13 +0800

    move editor to another project

--- editor/app/main.js
+++
@@ -1,8 +0,0 @@
-const {app, BrowserWindow} = require("electron")
-
-let mainWindow = null;
-
-app.on("ready", () => {
-    mainWindow = new BrowserWindow();
-    mainWindow.webContents.loadURL("file://" + __dirname + "/index.html")
-});
