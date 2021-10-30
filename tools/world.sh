f321a186235390e32ee4c312531f278f5421bb72
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2021-10-30 16:16:50 +0800

    rewrite world tool

--- tools/world.sh
+++ tools/world.sh
@@ -1,3 +1,16 @@
 read -p "World Name: " world_name
-cp "../resources/config/worlds/template.json" "../resources/config/worlds/${world_name}.json"
+echo "
+{
+    \"world-name\": \"{{world_name}}\",
+    \"background-color\": \"#000000FF\",
+    \"camera\": {
+      \"x\": 512,
+      \"y\": 384
+    },
+    \"children\": [
+    ],
+    \"components\": [
+    ]
+}
+" >> "../resources/config/worlds/${world_name}.json"
 sed -i "s/{{world_name}}/${world_name}/g" "../resources/config/worlds/${world_name}.json"
