9d3e2372148db87363044c099f73003696bff760
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2021-11-06 13:23:56 +0800

    fixed publish

--- tools/publish.sh
+++ tools/publish.sh
@@ -2,64 +2,68 @@
 
 echo "Change build mode..."
 sed -i "s/static constexpr short CURRENT_MODE = MODE_DEBUG;/static constexpr short CURRENT_MODE = MODE_RELEASE;/g" "../kernel/settings.h"
-sed -i "s/# set(CMAKE_CXX_FLAGS/set(CMAKE_CXX_FLAGS/g" "../CMakeLists.txt"
+sed -i "s/if (0)/if (1)/g" "../CMakeLists.txt"
 cd ..
 
-cmake --build . --target all -- -j6
+if  [ ! -d "build" ]; then
+  mkdir "build"
+fi
+
+cmake --build cmake-build-debug --target all -- -j6
 make
 
-for file in `find ./bin/resources -name "*.lua"`
+for file in `find ./build/resources -name "*.lua"`
 do
     echo "encrypt ${file}..."
-    `bin/crypto ${file} ${file: 0:${#file} - 4}".lsm"`
+    `build/crypto ${file} ${file: 0:${#file} - 4}".lsm"`
     rm ${file}
 done
 
-for file in `find ./bin/resources -name "*.json"`
+for file in `find ./build/resources -name "*.json"`
 do
     echo "encrypt ${file}..."
-    `bin/crypto ${file} ${file: 0:${#file} - 5}".cson"`
+    `build/crypto ${file} ${file: 0:${#file} - 5}".cson"`
     rm ${file}
 done
 
-for file in `find ./bin/resources -name "*.png"`
+for file in `find ./build/resources -name "*.png"`
 do
     echo "compress ${file}..."
-    `bin/compress ${file} ${file: 0:${#file} - 4}".cng"`
+    `build/compress ${file} ${file: 0:${#file} - 4}".cng"`
     rm ${file}
 done
 
-for file in `find ./bin/resources -name "*.jpg"`
+for file in `find ./build/resources -name "*.jpg"`
 do
     echo "compress ${file}..."
-    `bin/compress ${file} ${file: 0:${#file} - 4}".cpg"`
+    `build/compress ${file} ${file: 0:${#file} - 4}".cpg"`
     rm ${file}
 done
 
-for file in `find ./bin/resources -name "*.vs"`
+for file in `find ./build/resources -name "*.vs"`
 do
     echo "encrypt ${file}..."
-    `bin/crypto ${file} ${file: 0:${#file} - 3}".cs"`
+    `build/crypto ${file} ${file: 0:${#file} - 3}".cs"`
     rm ${file}
 done
 
-for file in `find ./bin/resources -name "*.frag"`
+for file in `find ./build/resources -name "*.frag"`
 do
     echo "encrypt ${file}..."
-    `bin/crypto ${file} ${file: 0:${#file} - 5}".crag"`
+    `build/crypto ${file} ${file: 0:${#file} - 5}".crag"`
     rm ${file}
 done
 
-for file in `find ./bin/resources -name "*.ini"`
+for file in `find ./build/resources -name "*.ini"`
 do
     echo "encrypt ${file}..."
-    `bin/crypto ${file} ${file: 0:${#file} - 4}".cini"`
+    `build/crypto ${file} ${file: 0:${#file} - 4}".cini"`
     rm ${file}
 done
 
-rm "bin/crypto"
-rm "bin/compress"
+rm "build/crypto"
+rm "build/compress"
 
 cd tools
-sed -i "s/set(CMAKE_CXX_FLAGS/# set(CMAKE_CXX_FLAGS/g" "../CMakeLists.txt"
+sed -i "s/if (1)/if (0)/g" "../CMakeLists.txt"
 sed -i "s/static constexpr short CURRENT_MODE = MODE_RELEASE;/static constexpr short CURRENT_MODE = MODE_DEBUG;/g" "../kernel/settings.h"
